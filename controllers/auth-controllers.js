9968e820dfe708965b313a2d25e53ff5b584564c
Tetyana Vynnyk <kharchenko1988@bigmir.net>
2023-04-20 17:52:53 +0300

    Homework 5

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,91 +1,130 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const fs = require("fs/promises");
+const path = require("path");
+
+const Jimp = require("jimp");
 
 const { ctrlWrapper } = require("../utils");
 
-const {User} = require("../models/user");
+const { User } = require("../models/user");
 
 const { HttpError } = require("../helpers/HttpError");
 
-const {SECRET_KEY} = process.env;
+const { SECRET_KEY } = process.env;
 
-const register = async(req, res)=> {
-    const {email, password, subscription} = req.body;
-    const user = await User.findOne({email});
-    if(user) {
-        throw HttpError(409, "Email already in use");
-    }
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
-    const hashPassword = await bcrypt.hash(password, 10);
-    
-    const result = await User.create({...req.body, password: hashPassword});
+const register = async (req, res) => {
+  const { email, password, subscription } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email already in use");
+  }
 
-    res.status(201).json({
-        name: result.name,
-        email: result.email,
-        subscription
-    })
-}
+  const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-const login = async(req, res) => {
-    const {email, password, subscription} = req.body;
-    const user = await User.findOne({email});
-    if(!user) {
-        throw HttpError(401, "Email or password invalid");
-    }
-    const passwordCompare = await bcrypt.compare(password, user.password);
-    if(!passwordCompare) {
-        throw HttpError(401, "Email or password invalid");
-    }
+  const result = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
-    const payload = {
-        id: user._id,
-    }
+  res.status(201).json({
+    name: result.name,
+    email: result.email,
+    subscription,
+  });
+};
 
-    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
-    await User.findByIdAndUpdate(user._id, {token});
+const login = async (req, res) => {
+  const { email, password, subscription } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password invalid");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password invalid");
+  }
 
-    res.json({
-        token,
-        email,
-        subscription
-    })
-}
+  const payload = {
+    id: user._id,
+  };
 
-const getCurrent = async(req, res)=> {
-    const {name, email, subscription} = req.user;
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
 
-    res.json({
-        name,
-        email,
-        subscription
-    })
-}
+  res.json({
+    token,
+    email,
+    subscription,
+  });
+};
 
-const logout = async(req, res)=> {
-    const {_id} = req.user;
-    await User.findByIdAndUpdate(_id, {token: ""});
+const getCurrent = async (req, res) => {
+  const { name, email, subscription } = req.user;
 
-    res.status(204).json({
-        message: "Logout success"
-    })
-}
+  res.json({
+    name,
+    email,
+    subscription,
+  });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  res.status(204).json({
+    message: "Logout success",
+  });
+};
 
 const updateSubscription = async (req, res) => {
-    const {_id, subscription} = req.user;
+  const { _id, subscription } = req.user;
 
-    const result = await User.findByIdAndUpdate(_id, subscription);
-    if (!result) {
-        throw HttpError(404, `User with ${_id} not found`);
-    }
-    res.json(result);
+  const result = await User.findByIdAndUpdate(_id, subscription);
+  if (!result) {
+    throw HttpError(404, `User with ${_id} not found`);
+  }
+  res.json(result);
+};
 
-}
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, filename } = req.file;
+
+  Jimp.read(filename)
+    .then((filename) => {
+      return (
+        filename
+          .resize(250, 250) // resize
+          //   .quality(60) // set JPEG quality
+          //   .greyscale() // set greyscale
+          .write(filename)
+      ); // save
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  const avatarName = `${_id}_${filename}`;
+  const resultUpload = path.join(avatarsDir, avatarName);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", avatarName);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({ avatarURL });
+};
 
 module.exports = {
-    register: ctrlWrapper(register),
-    login: ctrlWrapper(login),
-    getCurrent: ctrlWrapper(getCurrent),
-    logout: ctrlWrapper(logout),
-    updateSubscription: ctrlWrapper(updateSubscription)
-}
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+  updateSubscription: ctrlWrapper(updateSubscription),
+  updateAvatar: ctrlWrapper(updateAvatar),
+};
--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -4,7 +4,7 @@ const ctrl = require("../../controllers/auth-controllers")
 
 const {validateBody} = require("../../utils");
 
-const {authenticate} = require("../../middlewares");
+const {authenticate, upload} = require("../../middlewares");
 
 const {schemas} = require("../../models/user");
 
@@ -21,5 +21,7 @@ router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
 
 router.patch("/", validateBody(schemas.validateJoiUserSubscription), ctrl.updateSubscription);
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 
 module.exports = router;
